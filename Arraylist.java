332b1a2b3303d8e00556671c6e6f1dea7b84d3d5
anandk <anandk@bitlasoft.com>
2023-01-08 13:10:45 +0530

    arraychanges

---
+++ Arraylist.java
@@ -0,0 +1,42 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Arraylist {
+	
+	public static void main(String[] args) {
+		
+		
+		TreeSet a1=new TreeSet<>();
+//		
+		
+		a1.add("samsung");
+		a1.add("nokia");
+		a1.add("redmi");
+		a1.add("appo");
+		a1.add("iphone");
+		a1.add(2);
+		
+		System.out.println(a1);
+	
+		
+		
+		
+		
+	
+		
+		
+		
+	}
+
+}
